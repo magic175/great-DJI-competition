3a8721a264d1e96b13b81149b842b31f0972e09c
HLdrz <3063096005@qq.com>
2026-03-26 22:15:31 +0800

    chore:移除环境配置文件，更新gitignore

--- basic_dev/setup.bash
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sleep 8
-cd /basic_dev
-source devel/setup.bash
-roslaunch basic_dev start.launch
--- basic_dev/setup_local.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd $(dirname "${BASH_SOURCE[0]}" ) && pwd )"
-
-export OpenCV_DIR=/usr/lib/x86_64-linux-gnu/cmake/opencv4
